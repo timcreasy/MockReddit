c83a2ba19c3493041947575a0eac819128ef5c33
Tim Creasy <timcreasy@me.com>
2016-09-15 14:19:58 -0500

    Added param routing for upvote/downvote posting

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,6 @@ const { Router } = require('express');
 const router = Router();
 const Story = require('../models/story');
 
-// router.get('/', (req, res) => {
-//   res.render('home');
-// });
-
 router.get('/', (req, res) => {
   Story
     .find()
@@ -18,34 +14,35 @@ router.get('/', (req, res) => {
 });
 
 router.param('storyID', function (req, res, next, storyID) {
-  console.log("IN PARAM", storyID);
   next();
 });
 
 router.post('/upvote/:storyID', (req, res) => {
 
-  // Story
-  //   .update({ "_id": req.params.storyID }, {$set: { upvotes: 10000}}, () => {
-  //     console.log("UPDATED");
-  //     res.send(201);
-  //   })
+    Story
+      .find({ "_id": req.params.storyID })
+      .then((story) => {
+          Story
+            .update({ "_id": req.params.storyID }, {$set: { upvotes: story[0].upvotes + 1 }})
+            .then(() => {
+              res.redirect('/');
+            })
+      });
 
-  Story
-    .update({ "_id": req.params.storyID }, {$set: { upvotes: 11111}})
-    .then((data) => {
-      console.log("MY DATA", data);
-    })
-    .then(() => {
-      res.redirect('/');
-    })
-
-
-  // next();
 });
 
-router.post('/downvote/:storyID', (req, res, next) => {
-  console.log(req.params.storyID);
-  next();
+router.post('/downvote/:storyID', (req, res) => {
+
+    Story
+      .find({ "_id": req.params.storyID })
+      .then((story) => {
+          Story
+            .update({ "_id": req.params.storyID }, {$set: { upvotes: story[0].upvotes - 1 }})
+            .then(() => {
+              res.redirect('/');
+            })
+      });
+
 });
 
 
