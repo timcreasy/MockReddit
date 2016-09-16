e27f73b0f20b88f6cbd411241da79527f31e3e1d
Dave Harms <daveharmswebdev@gmail.com>
2016-09-16 12:22:53 -0500

    altered database.js to reflect herkoku deployment and link to mlab db

--- src/database.js
+++ src/database.js
@@ -2,7 +2,8 @@
 
 const mongoose = require('mongoose');
 
-const MONGOOSE_DB_URL = 'mongodb://localhost:27017/reddit';
+const MONGOOSE_DB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/reddit'
+// const MONGOOSE_DB_URL = 'mongodb://username:password@ds033096.mlab.com:33096/database0327';
 
 mongoose.Promise = Promise;
 
